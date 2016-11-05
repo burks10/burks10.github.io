b8843d3a8c205b5ae93b43a9f42be173a127b621
burks10 <compscistudio@gmail.com>
2016-11-05 15:35:33 -0400

    Adds cool scrolly text

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 $(function(){
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    var terms = [
+        "Web Developer",
+        "Car Enthusiast",
+        "Game Developer",
+        "Arduino Master",
+        "Software Developer"
+    ];
+    var currentTerm = 0;
+
+    // Transition terms
+    var tid = setTimeout(switchTerm, 3000);
+    function switchTerm() {
+        // Fade out the current term
+        $(".terms").addClass("animated fadeOutLeft").one(animationEnd, function() {
+            $(".terms").removeClass("animated fadeOutLeft");
+            $(".terms").html(terms[currentTerm]);
+            currentTerm += 1;
+            if (currentTerm == terms.length) {
+                currentTerm = 0;
+            }
+            // Fade in the new term
+            $(".terms").addClass("animated fadeInRight").one(animationEnd, function() {
+                $(".terms").removeClass("animated fadeInRight");
+                tid = setTimeout(switchTerm, 2000); // repeat myself
+            });
+        });
+    }
 
     // Hide the mobile menu item by default (let the media query figure it out.)
     $(".mobile-menu-item").hide();
